8557459e700e9fd5a03acabcbdb93ad7e4f52db4
Derek Harnett <derek.harnett@shaw.ca>
2024-04-13 15:41:12 -0700

    add script to count the rows of all tables

---
+++ sql/1-init/count-rows.sql
@@ -0,0 +1,7 @@
+/* Count the number of rows in each table of the ecommerce database. */
+
+select count(*) from all_sessions;
+select count(*) from analytics;
+select count(*) from products;
+select count(*) from sales_by_sku;
+select count(*) from sales_report;
